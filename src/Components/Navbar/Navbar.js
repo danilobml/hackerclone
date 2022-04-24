40b5d0ade27911556323c756011e8247a6d08a36
danilobml <danbarml@gmail.com>
2022-04-24 20:43:20 +0200

    styling

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import "./Navbar.css";
-import icon from './icon.svg';
-
+import icon from "./icon.svg";
+import { FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -8,17 +8,13 @@ const Navbar = () => {
       <div className="links">
         <img src={icon} className="nav-icon" alt="HackerNews icon" />
         <h1>Hacker News</h1>
-        <a href="#">new</a> |
-        <a href="#">past</a> |
-        <a href="#">comments</a> |
-        <a href="#">ask</a> |
-        <a href="#">show</a> |
-        <a href="#">jobs</a> |
-        <a href="#">submit</a>
+        <a href="#">new</a> |<a href="#">past</a> |<a href="#">comments</a> |<a href="#">ask</a> |<a href="#">show</a> |<a href="#">jobs</a> |<a href="#">submit</a>
       </div>
       <div>
-      <input type="text" placeholder="Search" />
-      <button type="submit"><i className="fa fa-search"></i></button>
+        <input type="text" placeholder="Search" />
+        <button type="submit" className="search-button">
+          <FaSearch className="searcg-icon" />
+        </button>
       </div>
     </div>
   );
